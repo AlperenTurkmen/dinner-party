bf6833443460fffd050836db0eedd5479b613276
Alperen Türkmen <57950250+AlperenTurkmen@users.noreply.github.com>
2023-04-02 13:00:09 +0100

    Update tracks.go
    read and list works, but at the same time.

--- tracks.go
+++ tracks.go
@@ -1,48 +1,73 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func tracksHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/tracks" {
-		http.Error(w, "YOK BRO", http.StatusNotFound)
+type Track struct {
+	ID    string `json:"Id"`
+	Audio string `json:"Audio"`
+}
+type Tracks []Track
+
+func tracksLister(w http.ResponseWriter, r *http.Request) {
+	var allTracks Tracks
+
+	// Read existing tracks from the file
+	allTracks, err := readTracksFromFile("tracks.json")
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error reading tracks from file.", http.StatusInternalServerError)
 		return
 	}
+	//find track ID from URL
+	parts := strings.Split(r.URL.Path, "/")
+	trackID := parts[len(parts)-1]
+	fmt.Fprintf(w, "TRACK ID=")
+	fmt.Fprintf(w, trackID)
 
-	if r.Method == "POST" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
+	//Show track from ID
+	for _, singleTrack := range allTracks {
+		if singleTrack.ID == trackID {
+			json.NewEncoder(w).Encode(singleTrack.Audio)
+		}
+	}
+	//Return all tracks
+	jsonBytes, err := json.Marshal(allTracks)
+	if err != nil {
+		// handle error
+	}
+	fmt.Fprintf(w, string(jsonBytes))
+
+	return
+
+}
+func readTracksFromFile(filename string) ([]Track, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
 
-	fmt.Fprintf(w, "Tracks will be listed here.")
+	var tracks []Track
+	err = json.Unmarshal(data, &tracks)
+	if err != nil {
+		return nil, err
+	}
+
+	return tracks, nil
 }
 
 func main() {
 
-	http.HandleFunc("/tracks", tracksHandler) // Update this line of code
+	http.HandleFunc("/tracks/", tracksLister)
 
 	fmt.Printf("Starting server at port 3000\n")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
-
 	}
-	fmt.Println("Go MySQL Tutorial")
-
-	// Open up our database connection.
-	// I've set up a database on my local machine using phpmyadmin.
-	// The database is called testDb
-	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/test")
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// defer the close till after the main function has finished
-	// executing
-	defer db.Close()
 }
